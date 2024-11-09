235b806953e2fd1ed44589a94e9a18e5eec8b88f
jhernandez4 <jokhernandez120744@gmail.com>
2024-11-08 17:51:19 -0800

    Add endpoint to get captions given a YouTube videoid

--- app.py
+++ app.py
@@ -1,7 +1,24 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+import httpx
+import os
+from youtube_transcript_api import YouTubeTranscriptApi, YouTubeRequestFailed
 
 app = FastAPI()
 
 @app.get('/')
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/get-captions/")
+async def get_captions(video_id: str):
+    response = YouTubeTranscriptApi.get_transcript(video_id)
+    
+    caption_transcript = ""
+
+    for segment in response:
+        caption_text = ' ' + segment['text'].replace('\n', ' ') + ' '
+        caption_transcript = caption_transcript + caption_text
+
+        print(segment['text'])
+
+    return caption_transcript 
