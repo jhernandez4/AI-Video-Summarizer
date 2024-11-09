b36f83aa505dbdbddd9763e6cc988357dcf7352d
jhernandez4 <jokhernandez120744@gmail.com>
2024-11-08 18:26:50 -0800

    Add function for extracting videoID from a YouTube link

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, HTTPException
 import httpx
 import os
 from youtube_transcript_api import YouTubeTranscriptApi, YouTubeRequestFailed
+import re
 
 app = FastAPI()
 
@@ -22,3 +23,17 @@ async def get_captions(video_id: str):
         print(segment['text'])
 
     return caption_transcript 
+
+def extract_video_id(input_str: str) -> str:
+    # Check if the input is already a valid video ID
+    if re.match(r'^[0-9A-Za-z_-]{11}$', input_str):
+        return input_str
+
+    # Regular expression to capture video ID from web or mobile URL formats
+    pattern = r'(?:v=|\/)([0-9A-Za-z_-]{11})'
+    match = re.search(pattern, input_str)
+
+    if match:
+        return match.group(1)
+    else:
+        raise HTTPException(status_code=400, detail="Invalid input format for YouTube video ID")
